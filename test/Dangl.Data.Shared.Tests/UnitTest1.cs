13ef3744651983fddc3595c47fe2f74a14728c39
Georg Dangl <georg@dangl.me>
2017-11-10 22:59:19 +0100

    Add test project

---
+++ test/Dangl.Data.Shared.Tests/UnitTest1.cs
@@ -0,0 +1,14 @@
+using System;
+using Xunit;
+
+namespace Dangl.Data.Shared.Tests
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
